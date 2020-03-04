a60d5f0908a9e9e9d5a61d5e352a7c9474723222
Ethen Zhao <captainxero@foxmail.com>
2020-03-04 18:10:27 +0800

    init commit

---
+++ src/test/java/com/findpets/findpetswxmini/FindpetswxminiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findpets.findpetswxmini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindpetswxminiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
